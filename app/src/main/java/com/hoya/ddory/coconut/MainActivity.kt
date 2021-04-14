e39bdadb6813bf1fc53e5ef3eedc01aa00eb0c68
Jongho Park <tghbjhpark@gmail.com>
2021-04-14 21:20:43 +0900

    load key, secret from assets

--- app/src/main/java/com/hoya/ddory/coconut/MainActivity.kt
+++ app/src/main/java/com/hoya/ddory/coconut/MainActivity.kt
@@ -2,10 +2,35 @@ package com.hoya.ddory.coconut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getTokenFromAsset()
+    }
+
+    fun getTokenFromAsset() {
+        val assetmanager = resources.assets
+        var inputStream: InputStream
+        val key = ByteArray(32)
+        val secret = ByteArray(32)
+
+        inputStream = assetmanager.open("api_key")
+        inputStream.read(key)
+        inputStream.close()
+
+        inputStream = assetmanager.open("api_secret")
+        inputStream.read(secret)
+        inputStream.close()
+
+        Log.i(TAG, "${String(key)}, ${String(secret)}")
+    }
+
+    companion object {
+        private const val TAG = "[coconut]MainActivity"
     }
 }
