22bc368ae9392cad6231d4cd36c3711d4e810c79
Jongho Park <tghbjhpark@gmail.com>
2021-04-15 23:22:35 +0900

    fix ticker data

--- app/src/main/java/com/hoya/ddory/coconut/cloud/response/TickerPublic.kt
+++ app/src/main/java/com/hoya/ddory/coconut/cloud/response/TickerPublic.kt
@@ -8,13 +8,16 @@ data class TickerPublic(
 )
 
 data class TickerPublicData(
-    @SerializedName("order_currency") val orderCurrency: String,
-    @SerializedName("payment_currency") val paymentCurrency: String,
     @SerializedName("opening_price") val openingPrice: String,
     @SerializedName("closing_price") val closingPrice: String,
     @SerializedName("min_price") val minPrice: String,
     @SerializedName("max_price") val maxPrice: String,
-    @SerializedName("average_price") val averagePrice: String,
     @SerializedName("units_traded") val unitsTraded: String,
+    @SerializedName("acc_trade_value") val accTradeValue: String,
+    @SerializedName("prev_closing_price") val prevClosingPrice: String,
+    @SerializedName("units_traded_24H") val unitsTraded24H: String,
+    @SerializedName("acc_trade_value_24H") val accTradeValue24H: String,
+    @SerializedName("fluctate_24H") val fluctate24H: String,
+    @SerializedName("fluctate_rate_24H") val fluctateRate24H: String,
     @SerializedName("date") val date: String
 )
