40be02d10e95da725ec791c6ba2e4fba4ffe69fc
Jongho Park <tghbjhpark@gmail.com>
2021-04-16 20:56:00 +0900

    Add orderbook public interface

--- app/src/main/java/com/hoya/ddory/coconut/cloud/PublicApiInterface.kt
+++ app/src/main/java/com/hoya/ddory/coconut/cloud/PublicApiInterface.kt
@@ -1,5 +1,6 @@
 package com.hoya.ddory.coconut.cloud
 
+import com.hoya.ddory.coconut.cloud.response.OrderbookPublic
 import com.hoya.ddory.coconut.cloud.response.TickerPublic
 import io.reactivex.Single
 import retrofit2.http.GET
@@ -11,4 +12,10 @@ interface PublicApiInterface {
         @Path("orderCurrency") orderCurrency: String,
         @Path("paymentCurrency") paymentCurrency: String
     ) : Single<TickerPublic>
+
+    @GET("public/orderbook/{orderCurrency}_{paymentCurrency}")
+    fun getOrderbook(
+        @Path("orderCurrency") orderCurrency: String,
+        @Path("paymentCurrency") paymentCurrency: String
+    ) : Single<OrderbookPublic>
 }
