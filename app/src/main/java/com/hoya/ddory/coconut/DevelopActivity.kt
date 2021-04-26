7c706fc16b99bb8dc3e933cdde6ddaeb654ac9a6
Jongho Park <tghbjhpark@gmail.com>
2021-04-26 23:03:17 +0900

    Add develop activity

---
+++ app/src/main/java/com/hoya/ddory/coconut/DevelopActivity.kt
@@ -0,0 +1,11 @@
+package com.hoya.ddory.coconut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DevelopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_develop)
+    }
+}
