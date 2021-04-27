e12fb90bec930cd583e59a804e90ae907f357879
Jongho Park <tghbjhpark@gmail.com>
2021-04-27 23:07:40 +0900

    Add account view

---
+++ app/src/main/java/com/hoya/ddory/coconut/view/account/AccountFragment.kt
@@ -0,0 +1,42 @@
+package com.hoya.ddory.coconut.view.account
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hoya.ddory.coconut.databinding.FragmentAccountBinding
+
+class AccountFragment : Fragment() {
+
+    private lateinit var viewDataBinding: FragmentAccountBinding
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = FragmentAccountBinding.inflate(inflater, container, false).apply {
+            viewModel = AccountViewModel()
+        }
+        return viewDataBinding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewDataBinding.viewModel?.onResume(requireContext())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewDataBinding.viewModel?.onPause()
+    }
+
+    companion object {
+        fun newInstance() = AccountFragment()
+    }
+}
---
+++ app/src/main/java/com/hoya/ddory/coconut/view/account/AccountViewModel.kt
@@ -0,0 +1,58 @@
+package com.hoya.ddory.coconut.view.account
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.hoya.ddory.coconut.model.BithumbModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.io.InputStream
+
+class AccountViewModel : ViewModel() {
+    private val _krw = MutableLiveData<String>().apply { value = "krw" }
+    val krw: LiveData<String>
+        get() = _krw
+
+    private val _xrp = MutableLiveData<String>().apply { value = "xrp" }
+    val xrp: LiveData<String>
+        get() = _xrp
+
+    fun onResume(context: Context) {
+        val assetmanager = context.resources.assets
+        var inputStream: InputStream
+        val key = ByteArray(32)
+        val secret = ByteArray(32)
+
+        inputStream = assetmanager.open("api_key")
+        inputStream.read(key)
+        inputStream.close()
+
+        inputStream = assetmanager.open("api_secret")
+        inputStream.read(secret)
+        inputStream.close()
+        BithumbModel(String(key), String(secret))
+            .getBalance()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    Log.i(TAG, "krw:${it.data.krw},xrp:${it.data.xrp}")
+                    _krw.value = it.data.krw
+                    _xrp.value = it.data.xrp
+                },
+                {
+
+                }
+            )
+    }
+
+    fun onPause() {
+
+    }
+
+    companion object {
+        private const val TAG = "[coconut]AccountViewModel"
+    }
+}
