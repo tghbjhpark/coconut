64ece416ae1a526055f416753019c04bda6ee931
Jongho <tghbjhpark@gmail.com>
2021-05-18 21:39:40 +0900

    modify import

--- app/src/main/java/com/hoya/ddory/coconut/view/account/AccountFragment.kt
+++ app/src/main/java/com/hoya/ddory/coconut/view/account/AccountFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.hoya.ddory.coconut.databinding.FragmentAccountBinding
 
