407171e9bfedd9a8dc063a4af0564af60c8524a0
Jongho <tghbjhpark@gmail.com>
2022-05-22 22:34:56 +0900

    Add account response ktor

---
+++ app/src/main/java/com/hoya/ddory/coconut/cloud/ktor/response/Account.kt
@@ -0,0 +1,19 @@
+package com.hoya.ddory.coconut.cloud.response.ktor
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Account(
+    val status: String,
+    val data: AccountData
+)
+
+@Serializable
+data class AccountData(
+    val created: String,
+    val account_id: String,
+    val order_currency: String,
+    val payment_currency: String,
+    val trade_fee: String,
+    val balance: String
+)
