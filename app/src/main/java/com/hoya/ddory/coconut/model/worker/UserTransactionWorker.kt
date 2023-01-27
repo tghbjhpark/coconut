32044b112e93f3408cd00a2199bbfd44f53e8c72
Jongho <tghbjhpark@gmail.com>
2023-01-28 00:39:48 +0900

    Add timeout in userTransaction

--- app/src/main/java/com/hoya/ddory/coconut/model/worker/UserTransactionWorker.kt
+++ app/src/main/java/com/hoya/ddory/coconut/model/worker/UserTransactionWorker.kt
@@ -8,69 +8,102 @@ import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.hoya.ddory.coconut.model.BithumbModel
 import com.hoya.ddory.coconut.model.DatabaseModel
+import kotlinx.coroutines.withTimeout
 import java.util.concurrent.TimeUnit
 
 class UserTransactionWorker(
     appContext: Context,
     workerParameters: WorkerParameters
-): CoroutineWorker(appContext, workerParameters) {
+) : CoroutineWorker(appContext, workerParameters) {
 
     override suspend fun doWork(): Result {
         Log.i("JONGHO", "UserTransactionWorker")
         val id = inputData.getString(USER_TRANSACTION_ID).orEmpty()
         val accountId = inputData.getInt(USER_TRANSACTION_ACCOUNT_ID, -1)
         val count = inputData.getInt(USER_TRANSACTION_COUNT, 0)
-        BithumbModel(applicationContext)
-            .getOrdersKtor(id, "XRP")
-            .apply {
-                if (data.order_status != "Completed" && count < 10) {
-                    val requester = OneTimeWorkRequestBuilder<UserTransactionWorker>()
-                        .setInputData(WorkerManager.userTransactionWorkerData(accountId, id, count + 1))
-                        .setInitialDelay(1L, TimeUnit.MINUTES)
-                        .addTag("account_id_$id")
-                        .build()
-                    WorkManager.getInstance(applicationContext).enqueue(requester)
-                } else {
-                    if (data.order_status == "Completed") {
-                        val database = DatabaseModel(applicationContext)
-                        val accountInfo = database.getAccount(accountId)
-                        if (data.type == "bid") {
-                            // buy
-                            var currentTotal = accountInfo.average.toFloat() * accountInfo.quantity.toFloat()
-                            val orderTotal = data.order_qty.toFloat() * data.order_price.toFloat()
-
-                            var totalFee = 0f
-                            data.contract.forEach {
-                                totalFee += it.fee.toFloat()
-                            }
-                            accountInfo.fee = (accountInfo.fee.toFloat() + totalFee).toString()
-                            accountInfo.available = (accountInfo.available.toFloat() - orderTotal - totalFee).toString()
-                            accountInfo.quantity = (accountInfo.quantity.toFloat() + data.order_qty.toFloat()).toString()
-                            accountInfo.average = ((currentTotal + orderTotal) / (accountInfo.quantity.toFloat())).toString()
+        kotlin.runCatching {
+            withTimeout(10_000L) {
+                BithumbModel(applicationContext)
+                    .getOrdersKtor(id, "XRP")
+                    .apply {
+                        if (data.order_status != "Completed" && count < 10) {
+                            val requester = OneTimeWorkRequestBuilder<UserTransactionWorker>()
+                                .setInputData(
+                                    WorkerManager.userTransactionWorkerData(
+                                        accountId,
+                                        id,
+                                        count + 1
+                                    )
+                                )
+                                .setInitialDelay(3L, TimeUnit.MINUTES)
+                                .addTag("account_id_$accountId")
+                                .build()
+                            WorkManager.getInstance(applicationContext).enqueue(requester)
                         } else {
-                            // sell
-                            var totalFee = 0f
-                            data.contract.forEach {
-                                totalFee += it.fee.toFloat()
-                            }
-                            val orderTotal = data.order_qty.toFloat() * data.order_price.toFloat()
-                            accountInfo.fee = (accountInfo.fee.toFloat() + totalFee).toString()
-                            accountInfo.quantity = "0"
-                            accountInfo.average = "0"
-                            accountInfo.available = (accountInfo.available.toFloat() + orderTotal - totalFee).toString()
-                        }
-                        database.updateAccount(accountInfo)
+                            if (data.order_status == "Completed") {
+                                val database = DatabaseModel(applicationContext)
+                                val accountInfo = database.getAccount(accountId)
+                                if (data.type == "bid") {
+                                    // buy
+                                    var currentTotal =
+                                        accountInfo.average.toFloat() * accountInfo.quantity.toFloat()
+                                    val orderTotal =
+                                        data.order_qty.toFloat() * data.order_price.toFloat()
 
-                        val inputData = WorkerManager.automationWorkerData(accountId)
-                        val requester = OneTimeWorkRequestBuilder<AutomationWorker>()
-                            .setInputData(inputData)
-                            .setInitialDelay(1L, TimeUnit.HOURS)
-                            .addTag("account_id_$id")
-                            .build()
-                        WorkManager.getInstance(applicationContext).enqueue(requester)
+                                    var totalFee = 0f
+                                    data.contract.forEach {
+                                        totalFee += it.fee.toFloat()
+                                    }
+                                    accountInfo.fee =
+                                        (accountInfo.fee.toFloat() + totalFee).toString()
+                                    accountInfo.available =
+                                        (accountInfo.available.toFloat() - orderTotal - totalFee).toString()
+                                    accountInfo.quantity =
+                                        (accountInfo.quantity.toFloat() + data.order_qty.toFloat()).toString()
+                                    accountInfo.average =
+                                        ((currentTotal + orderTotal) / (accountInfo.quantity.toFloat())).toString()
+                                } else {
+                                    // sell
+                                    var totalFee = 0f
+                                    data.contract.forEach {
+                                        totalFee += it.fee.toFloat()
+                                    }
+                                    val orderTotal =
+                                        data.order_qty.toFloat() * data.order_price.toFloat()
+                                    accountInfo.fee =
+                                        (accountInfo.fee.toFloat() + totalFee).toString()
+                                    accountInfo.quantity = "0"
+                                    accountInfo.average = "0"
+                                    accountInfo.available =
+                                        (accountInfo.available.toFloat() + orderTotal - totalFee).toString()
+                                }
+                                database.updateAccount(accountInfo)
+
+                                val inputData = WorkerManager.automationWorkerData(accountId)
+                                val requester = OneTimeWorkRequestBuilder<AutomationWorker>()
+                                    .setInputData(inputData)
+                                    .setInitialDelay(1L, TimeUnit.HOURS)
+                                    .addTag("account_id_$accountId")
+                                    .build()
+                                WorkManager.getInstance(applicationContext).enqueue(requester)
+                            }
+                        }
                     }
-                }
             }
+        }.onFailure {
+            val requester = OneTimeWorkRequestBuilder<UserTransactionWorker>()
+                .setInputData(
+                    WorkerManager.userTransactionWorkerData(
+                        accountId,
+                        id,
+                        count + 1
+                    )
+                )
+                .setInitialDelay(3L, TimeUnit.MINUTES)
+                .addTag("account_id_$accountId")
+                .build()
+            WorkManager.getInstance(applicationContext).enqueue(requester)
+        }
 
         return Result.success()
     }
